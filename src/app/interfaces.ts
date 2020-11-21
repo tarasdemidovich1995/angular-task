4e56174a0e2f7efeb6ca066b363e3aac98ed9851
Taras <tarasdemidovich1995@gmail.com>
2020-11-21 18:59:29 +0300

    feat: set up project structure

---
+++ src/app/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Course {
+  id: string;
+  title: string;
+  creationDate: Date;
+  duration: number;
+  description: string;
+}
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
---
+++ src/app/pages/create-course-page/create-course-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-course-page',
+  templateUrl: './create-course-page.component.html',
+  styleUrls: ['./create-course-page.component.scss']
+})
+export class CreateCoursePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
